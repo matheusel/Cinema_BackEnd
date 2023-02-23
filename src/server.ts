21258b5c7966842fc4bca0e237887071fdf95d87
Beatriz <biaanjos1830@icloud.com>
2023-02-23 11:43:43 -0300

    banco de dados usuarios

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import fastify from "fastify";
 import { transactionsRouteSeries } from "./rotas/series";
 import { transactionsRouteFilmes } from "./rotas/filmes";
 import { transactionsRouteAvaliacao } from "./rotas/avaliacao";
+import { transactionsRouteUsuarios } from "./rotas/usuarios";
+
 
 const app = fastify();
 
-app.register(transactionsRouteSeries, transactionsRouteFilmes, transactionsRouteAvaliacao);
+app.register(transactionsRouteSeries, transactionsRouteFilmes, transactionsRouteAvaliacao, transactionsRouteUsuarios);
 
 app.listen({
     port:3333,
